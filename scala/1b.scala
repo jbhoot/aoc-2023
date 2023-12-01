07db767d0c19e4e3cae426979e927c5e20617095
Jayesh Bhoot <jysh@jysh.net>
2023-12-01 22:33:51 +0530

    day 1 solution in scala

---
+++ scala/1b.scala
@@ -0,0 +1,54 @@
+val tokens = List(
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "0",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+  "zero"
+)
+
+val toDigit: String => String = token =>
+  token match
+    case "one" | "1" => "1"
+    case "two" | "2" => "2"
+    case "three" | "3" => "3"
+    case "four" | "4" => "4"
+    case "five" | "5" => "5"
+    case "six" | "6" => "6"
+    case "seven" | "7" => "7"
+    case "eight" | "8" => "8"
+    case "nine" | "9" => "9"
+    case "zero" | "0" => "0"
+    case _ => throw IllegalArgumentException(s"Invalid string ${token}")
+
+val parseLine: String => Int = (line) =>
+  val (first, firstI) = tokens
+    .map(t => (t, line.indexOf(t)))
+    .filter((t, i) => i >= 0)
+    .minBy((t, i) => i)
+  val (last, lastI) = tokens
+    .map(t => (t, line.lastIndexOf(t)))
+    .filter((t, i) => i >= 0)
+    .maxBy((t, i) => i)
+  val firstLast = toDigit(first) + toDigit(last)
+  firstLast.toInt
+
+@main
+def main(): Unit =
+  val src = scala.io.Source.fromFile("../input/1.txt")
+  val lines = src.getLines()
+  println(lines.map(parseLine).sum)
